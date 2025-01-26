c4cb2c42570aeb6655b1cd1f3bce771df76dc8a6
Joel Mislav Kunst <7sedam7@gmail.com>
2025-01-26 13:10:11 +0200

    Improved implementation of where contition execution, addad structure, started implementing specific operators

--- src/libs/executor.rs
+++ src/libs/executor.rs
@@ -1,7 +1,9 @@
 use std::error::Error;
 use std::path::PathBuf;
+use std::usize;
 
 use gray_matter::Pod;
+use hashbrown::HashSet;
 
 use crate::libs::data_fetcher::fetch_data;
 use crate::libs::parser::{ExpressionElement, FieldValue, Operator, Query};
@@ -35,7 +37,7 @@ pub fn execute_query(
     println!("Parsed query: {:?}", query);
     let frontmatter_data = fetch_data(&query.from_function.unwrap())?;
 
-    execute_where(&query.where_expression, &frontmatter_data);
+    execute_where(&query.where_expression, &frontmatter_data)?;
 
     //for (path, frontmatter) in frontmatter_data {
     //    println!("File: {}", path.display());
@@ -47,59 +49,19 @@ pub fn execute_query(
     Ok(vec![])
 }
 
-fn execute_where(condition: &Vec<ExpressionElement>, data: &Vec<(PathBuf, Pod)>) {
-    let queue = infix_to_postfix(condition);
-    let mut eval_stack = Vec::new();
-
-    println!("QUEUE: {:?}", queue);
-
-    for element in queue {
-        match element {
-            ExpressionElement::FieldName(_)
-            | ExpressionElement::FieldValue(_)
-            | ExpressionElement::Function(_) => {
-                eval_stack.push(element.clone());
-            }
-            ExpressionElement::Operator(op) => {
-                let right = eval_stack.pop().unwrap();
-                let left = eval_stack.pop().unwrap();
-                execute_operation(data, &op, &left, &right);
-                // need te return marked filtered data
-            }
-            _ => {} // consider throwing an error, even though this should not happen
-        }
-    }
+#[derive(Debug, PartialEq, Clone)]
+enum Operand {
+    QueueElement(ExpressionElement),
+    BoolElement(HashSet<usize>),
 }
 
-fn execute_operation(
+fn execute_where(
+    condition: &Vec<ExpressionElement>,
     data: &Vec<(PathBuf, Pod)>,
-    op: &Operator,
-    left: &ExpressionElement,
-    right: &ExpressionElement,
-) {
-    // TODO: Handle operator
-    match op {
-        Operator::And => println!("AND operator"),
-        Operator::Or => println!("OR operator"),
-        Operator::In => println!("IN operator"),
-        Operator::Lt => println!("LESS THAN operator"),
-        Operator::Lte => println!("LESS THAN OR EQUAL operator"),
-        Operator::Gt => println!("GREATER THAN operator"),
-        Operator::Gte => println!("GREATER THAN OR EQUAL operator"),
-        Operator::Eq => println!("EQUAL operator"),
-        Operator::Neq => println!("NOT EQUAL operator"),
-        Operator::Plus => println!("PLUS operator"),
-        Operator::Minus => println!("MINUS operator"),
-        Operator::Multiply => println!("MULTIPLY operator"),
-        Operator::Divide => println!("DIVIDE operator"),
-        Operator::Power => println!("POWER operator"),
-        Operator::FloorDivide => println!("FLOOR DIVIDE operator"),
-    }
-}
-
-fn infix_to_postfix(condition: &Vec<ExpressionElement>) -> Vec<ExpressionElement> {
+) -> Result<(), String> {
     let mut stack = Vec::new();
-    let mut queue = Vec::new();
+    let mut eval_stack = Vec::new();
+    let mut bool_stack = Vec::new();
 
     // Define operator precedence
     let operator_precedence = |op: &Operator| match op {
@@ -124,29 +86,161 @@ fn infix_to_postfix(condition: &Vec<ExpressionElement>) -> Vec<ExpressionElement
             ExpressionElement::FieldName(_)
             | ExpressionElement::FieldValue(_)
             | ExpressionElement::Function(_) => {
-                queue.push(element.clone());
+                eval_stack.push(element.clone());
             }
             ExpressionElement::Operator(op) => {
-                // op goes on stack unless op on stack has higher priority, then it goes to queue
+                // op goes on stack, but if stack has equal or higher priority operator, that one
+                // goes from stack to the "queue"
                 if let Some(ExpressionElement::Operator(last_op)) = stack.last() {
-                    // compare and higher priority goes to queue
                     if operator_precedence(last_op) >= operator_precedence(op) {
-                        queue.push(stack.pop().unwrap());
+                        handle_operator_to_queue(
+                            &mut stack,
+                            &mut eval_stack,
+                            &mut bool_stack,
+                            data,
+                        )?;
                     }
                 }
                 stack.push(element.clone());
             }
             ExpressionElement::ClosedBracket => {
                 while let Some(ExpressionElement::Operator(_)) = stack.last() {
-                    queue.push(stack.pop().unwrap());
+                    handle_operator_to_queue(&mut stack, &mut eval_stack, &mut bool_stack, data)?;
                 }
                 stack.pop();
             }
         }
     }
     while stack.last().is_some() {
-        queue.push(stack.pop().unwrap());
+        handle_operator_to_queue(&mut stack, &mut eval_stack, &mut bool_stack, data)?;
     }
 
-    queue
+    Ok(())
+}
+
+fn handle_operator_to_queue(
+    stack: &mut Vec<ExpressionElement>,
+    eval_stack: &mut Vec<ExpressionElement>,
+    bool_stack: &mut Vec<HashSet<usize>>,
+    data: &Vec<(PathBuf, Pod)>,
+) -> Result<(), String> {
+    let op;
+    let left;
+    let right;
+
+    if let Some(should_be_operator) = stack.pop() {
+        match should_be_operator {
+            ExpressionElement::Operator(_op) => {
+                op = _op.clone();
+
+                match _op {
+                    Operator::Or | Operator::And => {
+                        let _right = bool_stack.pop().ok_or(
+                            "Expected operand on the bool stack, but found nothing!".to_string(),
+                        )?;
+                        let _left = bool_stack
+                            .pop()
+                            .ok_or("Expected operand on the bool stack, but found nothing!")?;
+                        left = Operand::BoolElement(_left);
+                        right = Operand::BoolElement(_right);
+                    }
+                    _ => {
+                        let _right = eval_stack
+                            .pop()
+                            .ok_or("Expected operand on the eval stack, but found nothing!")?;
+                        let _left = eval_stack
+                            .pop()
+                            .ok_or("Expected operand on the eval stack, but found nothing!")?;
+                        left = Operand::QueueElement(_left);
+                        right = Operand::QueueElement(_right);
+                    }
+                }
+            }
+            _ => {
+                return Err(format!(
+                    "Expected operator, but found: {:?}",
+                    should_be_operator
+                ))
+            }
+        }
+    } else {
+        return Err("Expected operator on top of the stack, but found nothing!".to_string());
+    }
+
+    match execute_operation(data, &op, &left, &right)? {
+        Operand::BoolElement(set) => {
+            bool_stack.push(set);
+        }
+        Operand::QueueElement(value) => {
+            eval_stack.push(value);
+        }
+    }
+
+    Ok(())
+}
+
+fn execute_operation(
+    data: &Vec<(PathBuf, Pod)>,
+    op: &Operator,
+    left: &Operand,
+    right: &Operand,
+) -> Result<Operand, String> {
+    // TODO: Handle operator
+    match op {
+        // get bools, return bool
+        Operator::And => {
+            execute_bool_operation(|a, b| a.intersection(b).copied().collect(), left, right)
+        }
+        Operator::Or => execute_bool_operation(|a, b| a.union(b).copied().collect(), left, right),
+
+        // get values, return bools
+        Operator::In => Err("IN operator not implemented!".to_string()),
+        Operator::Lt => Err("LESS THAN operator not implemented!".to_string()),
+        Operator::Lte => Err("LESS THAN OR EQUAL operator not implemented!".to_string()),
+        Operator::Gt => Err("GREATER THAN operator not implemented!".to_string()),
+        Operator::Gte => Err("GREATER THAN OR EQUAL operator not implemented!".to_string()),
+        Operator::Eq => execute_operator_eq(data, left, right),
+        Operator::Neq => Err("NOT EQUAL operator not implemented!".to_string()),
+
+        // get values, return values
+        Operator::Plus => Err("PLUS operator not implemented!".to_string()),
+        Operator::Minus => Err("MINUS operator not implemented!".to_string()),
+        Operator::Multiply => Err("MULTIPLY operator not implemented!".to_string()),
+        Operator::Divide => Err("DIVIDE operator not implemented!".to_string()),
+        Operator::Power => Err("POWER operator not implemented!".to_string()),
+        Operator::FloorDivide => Err("FLOOR DIVIDE operator not imlemented!".to_string()),
+    }
+}
+
+fn execute_bool_operation(
+    op: fn(&HashSet<usize>, &HashSet<usize>) -> HashSet<usize>,
+    left: &Operand,
+    right: &Operand,
+) -> Result<Operand, String> {
+    let Operand::BoolElement(left_set) = left else {
+        return Err("Operation AND expects operands to be BoolElement, LEFT was not!".to_string());
+    };
+    let Operand::BoolElement(right_set) = right else {
+        return Err("Operation AND expects operands to be BoolElement, LEFT was not!".to_string());
+    };
+
+    Ok(Operand::BoolElement(op(left_set, right_set)))
+}
+
+fn execute_operator_eq(
+    data: &Vec<(PathBuf, Pod)>,
+    left: &Operand,
+    right: &Operand,
+) -> Result<Operand, String> {
+    let Operand::QueueElement(left_val) = left else {
+        return Err("Operation AND expects operands to be BoolElement, LEFT was not!".to_string());
+    };
+    let Operand::QueueElement(right_val) = right else {
+        return Err("Operation AND expects operands to be BoolElement, LEFT was not!".to_string());
+    };
+
+    let indexes: HashSet<usize> = HashSet::new();
+    // TODO: fill the indexes mased on data that satisfies the condition
+
+    Ok(Operand::BoolElement(indexes))
 }
