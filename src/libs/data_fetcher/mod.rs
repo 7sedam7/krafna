ed49dda6bb651645c895c2a2ba7dccc86a9233ba
Joel Mislav Kunst <7sedam7@gmail.com>
2025-02-27 22:11:44 +0200

    Added fetching of links and tasks

--- src/libs/data_fetcher/mod.rs
+++ src/libs/data_fetcher/mod.rs
@@ -12,6 +12,8 @@ use crate::libs::parser::Function;
 pub fn fetch_data(from_function: &Function) -> Result<Vec<Pod>, Box<dyn Error>> {
     match from_function.name.to_uppercase().as_str() {
         "FRONTMATTER_DATA" => markdown_fetcher::fetch_frontmatter_data(&from_function.args),
+        "MD_LINKS" => markdown_fetcher::fetch_markdown_links(&from_function.args),
+        "MD_TASKS" => markdown_fetcher::fetch_markdown_tasks(&from_function.args),
         _ => Err(format!("Unknown function: {}", from_function.name).into()),
     }
 }
